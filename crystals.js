06e0c9bb6f2895868c14bcccf7210ae5a1cd13fa
Maddy Gould <maddykgould@gmail.com>
2017-11-18 16:55:56 -0800

    second commit

--- crystals.js
+++ crystals.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
 	var randomNumber = Math.floor(Math.random() * 24) + 1;
 	//console.log('randomNumber', randomNumber);
 	$("#randomNumber").html(randomNumber);
-});
+
+var total=0;
+
+var wins=0;
+
+var losses=0;
 
 var crystalNumber1= Math.floor(Math.random() * 12) + 1;
 
@@ -14,59 +19,78 @@ var crystalNumber3= Math.floor(Math.random() * 12) + 1;
 
 var crystalNumber4= Math.floor(Math.random() * 12) + 1;
 
-var wins = 0;
-var losses = 0;
-//var score= [crystalNumber1,crystalNumber2,crystalNumber3,crystalNumber4];
-var result = 
 
-int score = 0;
-    function score() {
-document.getElementById("score").innerHTML = score;
-
-
-var score = crystalNumber1 + crystalNumber2 + crystalNumber3 + crystalNumber4;
-
-if (randomNumber = score) {
-	document.write("You Win!");
-	return 1;
+function checkWin (){
+if (total === randomNumber) {
+	wins++;
+	$(".wins").html("wins " + wins);
+	console.log("wins");
+	reset();
 }
 
-function updateScores(score, points) {
-    if(score === 1) {
-      wins += points;
-    }
-
+if (total > randomNumber) {
+	losses++;
+	$(".losses").html("Losses " + losses);
+	console.log("losses");
+	reset();
 }
-
+}
 
 //creates random number result for crystals. 
 $(".crystal1").on("click", function() {
 	console.log(crystalNumber1);
 	//puts crystals in score box
-	$(".score").html(crystalNumber1);
+	total += crystalNumber1 //update all
+	$(".score").html(total);
+	checkWin();
 });
 
 
 $(".crystal2").on("click", function() {
 	console.log(crystalNumber2);
-	$(".score").html(crystalNumber2);
+	total += crystalNumber2
+	$(".score").html(total);
+	checkWin();
 });
 
 $(".crystal3").on("click", function() {
 	console.log(crystalNumber3);
-	$(".score").html(crystalNumber3);
+	total += crystalNumber3
+	$(".score").html(total);
+	checkWin();
 });
 
 $(".crystal4").on("click", function() {
 	console.log(crystalNumber4);
-	$(".score").html(crystalNumber4);
+	total += crystalNumber4
+	$(".score").html(total);
+	checkWin();
+});
 
-//if score = $("#randomNumber", function() {
-//	wins++;
-//
+function reset () {
+	
+	randomNumber = Math.floor(Math.random() * 24) + 1;
+	
+	$("#randomNumber").html(randomNumber);
 
-  	
+	crystalNumber1= Math.floor(Math.random() * 12) + 1;
+
+	crystalNumber2= Math.floor(Math.random() * 12) + 1;
+
+	 crystalNumber3= Math.floor(Math.random() * 12) + 1;
+
+	 crystalNumber4= Math.floor(Math.random() * 12) + 1;
+
+	 total=0;
+
+	 $(".score").html(total);
+
+
+}
+});
+
+
+//reset crystals and random values
 
 
 
-//grab 
